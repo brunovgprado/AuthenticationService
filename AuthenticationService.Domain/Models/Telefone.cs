897e2275b8bdafaa73adeefcd70d48df61dc90e4
Bruno Prado <brunomcp2010@gmail.com>
2019-10-30 18:37:55 -0300

    add response view model, implemented authenticationcontext, models and servicebase
    Fix author name and email

--- AuthenticationService.Domain/Models/Telefone.cs
+++ AuthenticationService.Domain/Models/Telefone.cs
@@ -6,7 +6,8 @@ namespace AuthenticationService.Domain.Models
 {
     public class Telefone
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
+        public Guid UsuarioFK { get; set; }
         public virtual Usuario User { get; set; }
         public string Numero { get; set; }
         public string DDD { get; set; }
--- AuthenticationService.Domain/Models/Usuario.cs
+++ AuthenticationService.Domain/Models/Usuario.cs
@@ -12,7 +12,7 @@ namespace AuthenticationService.Domain.Models
         public string Senha { get; set; }
         public virtual List<Telefone> Telefones { get; set; }
         public DateTime DataCriacao { get; set; }
-        public DateTime DataAtualizacao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
         public DateTime UltimoLogin { get; set; }
         public string Token { get; set; }
     }
